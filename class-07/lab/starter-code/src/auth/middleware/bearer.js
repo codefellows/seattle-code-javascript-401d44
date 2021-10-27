47cac143d1ff91e45bcb8249ba8c3187b505d640
chattray <chea0073@gmail.com>
2021-10-26 19:17:35 -0700

    final attempt

--- class-07/lab/starter-code/src/auth/middleware/bearer.js
+++ class-07/lab/starter-code/src/auth/middleware/bearer.js
@@ -10,6 +10,7 @@ module.exports = async (req, res, next) => {
 
     const token = req.headers.authorization.split(' ').pop();
     const validUser = await users.authenticateToken(token);
+    
     req.user = validUser;
     req.token = validUser.token;
     next();
