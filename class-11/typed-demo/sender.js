1b869288327e337c6027020412baaba7430f485a
JacobKnaack <jacobknck@gmail.com>
2021-11-01 12:09:31 -0700

    updates send with comment about receiver function

--- class-11/typed-demo/sender.js
+++ class-11/typed-demo/sender.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const messenger = require('./messenger.js');
+// this will run all the code from receiver before we configure our input and emit our message to the event pool!
 require('./receiver.js');
 
 const input = process.argv[2];
